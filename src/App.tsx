451edb7ec954069fb2fda476d7fc24cf13ecac0b
mnacfil <mnacfil@gmail.com>
2024-02-09 09:31:19 +0800

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+Button;
+import Button from "./components/button";
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+};
+
+export default App;
